bbcf95ec5edb738787fa34d806e05be4b8c2238b
Asus <muammararianto201@gmail.com>
2023-12-04 16:05:19 +0800

    New API

--- configs/Database.js
+++ configs/Database.js
@@ -1,9 +1,13 @@
 import { Sequelize } from "sequelize";
 
 
-const db = new Sequelize("railway","root","gbGe26-525FfgGDCbAGFc61bcFAhH5H4",{
-    host: "roundhouse.proxy.rlwy.net",
-    dialect: "mysql",
+const db = new Sequelize({
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
+    dialect: 'mysql',
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST
 })
 
 export default db;
